5151dd2ddf50cd05278b55dfd2056ab5a51cd2e5
Eric Mootz <eric@mootzoid.com>
2015-03-17 17:59:01 +0100

    new command HelloKL that outputs Hello KL using a small graph

---
+++ src/cmd_HelloKL.cpp
@@ -0,0 +1,31 @@
+// ---------------------------------------------------------------
+// 
+// COMMAND:		HelloWorld.
+// 
+// ARGUMENTS:	none.
+//
+// NOTES:		- outputs "Hello World!" in the event log.
+//				- does not use any Fabric stuff.
+// 
+// ---------------------------------------------------------------
+
+#include "plugin.h"
+#include "cmd_HelloWorld.h"
+
+// static thingy for the log system.
+LXtTagInfoDesc cmd_HelloWorld::descInfo[] =
+{
+	{ LXsSRV_LOGSUBSYSTEM, LOG_SYSTEM_NAME },
+	{ 0 }
+};
+
+// execute code.
+void cmd_HelloWorld::cmd_Execute(unsigned flags)
+{
+	gLog.Message(LXe_INFO, SERVER_NAME, "Hello World!", " ");
+}
+ 
+// end of file
+
+
+

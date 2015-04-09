98f95b7815735b151e85f2ea2ccb32726549ed94
Eric Mootz <eric@mootzoid.com>
2015-04-09 15:51:48 +0200

    Import JSON (Fileselect Box) 2

---
+++ src/btn_dfgExportJSON.pl
@@ -0,0 +1,18 @@
+#perl
+
+use File::Basename;
+use File::Path;
+
+lx("dialog.setup fileSave");
+lx("dialog.title {Export JSON File}");
+lx("dialog.fileTypeCustom json JSON *.json json");
+lx("dialog.fileSaveFormat json extension");
+
+if(lx("dialog.open")) 
+{
+	my $File = lxq("dialog.result ?");
+
+	my $itemName = lxq("item.name ?");
+	lx("dfgExportJSON " . '"' . $itemName . '" "' . $File . '"');
+}
+
